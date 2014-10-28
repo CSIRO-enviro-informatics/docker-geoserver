03c11f76d0673f9f427b03dfa4676633543e8396
Jonathan Yu <yu021@javahost-dev.it.csiro.au>
2014-10-28 12:04:35 +1100

    removing run command in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+/bin/bash
 
 docker kill geoserver
 docker rm geoserver
 
 
 docker build -t csiro_env/geoserver .
-docker run --name "geoserver" -p 8080:8080 -d -t csiro_env/geoserver
-
 
